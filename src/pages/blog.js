7e3bba4229b7f7811891532ac9f8af6efc9b97bb
Olvine23 <olivinegeorge@gmal.com>
2022-03-01 17:10:47 -0800

    added  blog files

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,13 +1,35 @@
 import React from 'react'
+import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
-const BlogPage = () => {
+const BlogPage = ({data}) => {
   return (
      <>
      <Layout pageTitle="My Blogs" />
+     {data.allFile.nodes.map(
+         node => (
+             <li key ={node.name}>
+
+                 {node.name}
+
+             </li>
+         )
+     )}
      
      </>
   )
 }
+
+export const query = graphql`
+    query MyQuery {
+        allFile {
+        nodes {
+            name
+        }
+        }
+    }
+    
+
+    `
 
 export default BlogPage
